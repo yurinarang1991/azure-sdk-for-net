40a8f1e166f2ca11f08c454a86458405b460bc28
Abhipsa Misra <abhipsa.misra@microsoft.com>
2020-06-16 15:14:39 -0700

    feat(iot-service): Add additional constructor to take in SharedAccessKey

--- sdk/iot/Azure.Iot.Hub.Service/src/Authentication/IotHubConnectionString.cs
+++ sdk/iot/Azure.Iot.Hub.Service/src/Authentication/IotHubConnectionString.cs
@@ -1,62 +1,33 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
 using Azure.Core;
 
 namespace Azure.Iot.Hub.Service.Authentication
 {
     /// <summary>
-    /// Implementation for creating the shared access signature token provider.
+    /// Implementation for parsing the supplied IoT Hub connection string.
     /// </summary>
     internal class IotHubConnectionString
     {
         private const string HostNameIdentifier = "HostName";
         private const string SharedAccessKeyIdentifier = "SharedAccessKey";
         private const string SharedAccessKeyNameIdentifier = "SharedAccessKeyName";
-        private const string SharedAccessSignatureIdentifier = "SharedAccessSignature";
-
-        private readonly ConnectionString _connectionString;
-        private readonly string _sharedAccessPolicy;
-        private readonly string _sharedAccessKey;
-        private readonly string _sharedAccessSignature;
 
         internal IotHubConnectionString(string connectionString)
         {
-            _connectionString = ConnectionString.Parse(connectionString);
-            _sharedAccessKey = _connectionString.GetNonRequired(SharedAccessKeyIdentifier);
-            _sharedAccessPolicy = _connectionString.GetNonRequired(SharedAccessKeyNameIdentifier);
-            _sharedAccessSignature = _connectionString.GetNonRequired(SharedAccessSignatureIdentifier);
+            var iotHubConnectionString = ConnectionString.Parse(connectionString);
 
-            if (!ValidateInput(_sharedAccessPolicy, _sharedAccessKey, _sharedAccessSignature))
-            {
-                throw new ArgumentException("Specify either both the sharedAccessKey and sharedAccessKeyName, or only sharedAccessSignature");
-            }
+            SharedAccessKey = iotHubConnectionString.GetNonRequired(SharedAccessKeyIdentifier);
+            SharedAccessPolicy = iotHubConnectionString.GetNonRequired(SharedAccessKeyNameIdentifier);
 
-            HostName = _connectionString.GetRequired(HostNameIdentifier);
+            HostName = iotHubConnectionString.GetRequired(HostNameIdentifier);
         }
 
         internal string HostName { get; }
 
-        internal ISasTokenProvider GetSasTokenProvider()
-        {
-            if (_sharedAccessSignature == null)
-            {
-                return new SasTokenProviderWithSharedAccessKey(HostName, _sharedAccessPolicy, _sharedAccessKey);
-            }
-            return new FixedSasTokenProvider(_sharedAccessSignature);
-        }
+        internal string SharedAccessPolicy { get; }
 
-        private static bool ValidateInput(string sharedAccessPolicy, string sharedAccessKey, string sharedAccessSignature)
-        {
-            if (sharedAccessSignature == null)
-            {
-                return sharedAccessKey != null && sharedAccessPolicy != null;
-            }
-            else
-            {
-                return sharedAccessKey == null && sharedAccessPolicy == null;
-            }
-        }
+        internal string SharedAccessKey { get; }
     }
 }
--- sdk/iot/Azure.Iot.Hub.Service/src/IoTHubServiceClient.cs
+++ sdk/iot/Azure.Iot.Hub.Service/src/IoTHubServiceClient.cs
@@ -82,16 +82,87 @@ namespace Azure.Iot.Hub.Service
         /// </param>
         public IoTHubServiceClient(string connectionString, IoTHubServiceClientOptions options)
         {
+            Argument.AssertNotNull(connectionString, nameof(connectionString));
             Argument.AssertNotNull(options, nameof(options));
 
             var iotHubConnectionString = new IotHubConnectionString(connectionString);
-            ISasTokenProvider sasProvider = iotHubConnectionString.GetSasTokenProvider();
+            var sasTokenProvider = new SasTokenProviderWithSharedAccessKey(
+                iotHubConnectionString.HostName,
+                iotHubConnectionString.SharedAccessPolicy,
+                iotHubConnectionString.SharedAccessKey);
 
             _endpoint = BuildEndpointUriFromHostName(iotHubConnectionString.HostName);
 
             _clientDiagnostics = new ClientDiagnostics(options);
 
-            options.AddPolicy(new SasTokenAuthenticationPolicy(sasProvider), HttpPipelinePosition.PerCall);
+            options.AddPolicy(new SasTokenAuthenticationPolicy(sasTokenProvider), HttpPipelinePosition.PerCall);
+            _httpPipeline = HttpPipelineBuilder.Build(options);
+
+            _registryManagerRestClient = new RegistryManagerRestClient(_clientDiagnostics, _httpPipeline, _endpoint, options.GetVersionString());
+            _twinRestClient = new TwinRestClient(_clientDiagnostics, _httpPipeline, null, options.GetVersionString());
+            _deviceMethodRestClient = new DeviceMethodRestClient(_clientDiagnostics, _httpPipeline, _endpoint, options.GetVersionString());
+
+            Devices = new DevicesClient(_registryManagerRestClient, _twinRestClient, _deviceMethodRestClient);
+            Modules = new ModulesClient(_registryManagerRestClient, _twinRestClient, _deviceMethodRestClient);
+
+            Statistics = new StatisticsClient();
+            Messages = new CloudToDeviceMessagesClient();
+            Files = new FilesClient();
+            Jobs = new JobsClient();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IoTHubServiceClient"/> class.
+        /// </summary>
+        /// <param name="hostName">
+        /// The IoT Hub service instance host to connect to.
+        /// </param>
+        /// <param name="sharedAccessPolicy">
+        /// The IoT Hub access permission, which can be either "iothubowner", "service", "registryRead" or "registryReadWrite" policy, as applicable.
+        /// For more information, see <see href="https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-devguide-security#access-control-and-permissions"/>.
+        /// </param>
+        /// <param name="sharedAccessKey">
+        /// The IoT Hub shared access key associated with the shared access policy permissions."/>
+        /// </param>
+        public IoTHubServiceClient(string hostName, string sharedAccessPolicy, string sharedAccessKey)
+            : this(hostName, sharedAccessPolicy, sharedAccessKey, new IoTHubServiceClientOptions())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IoTHubServiceClient"/> class.
+        /// </summary>
+        /// <param name="hostName">
+        /// param name="hostName">
+        /// The IoT Hub service instance host to connect to.
+        /// </param>
+        /// <param name="sharedAccessPolicy">
+        /// The IoT Hub access permission, which can be either "iothubowner", "service", "registryRead" or "registryReadWrite" policy, as applicable.
+        /// For more information, see <see href="https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-devguide-security#access-control-and-permissions"/>.
+        /// </param>
+        /// <param name="sharedAccessKey">
+        /// The IoT Hub shared access key associated with the shared access policy permissions."/>
+        /// </param>
+        /// <param name="options">
+        /// Options that allow configuration of requests sent to the IoT Hub service.
+        /// </param>
+        public IoTHubServiceClient(string hostName, string sharedAccessPolicy, string sharedAccessKey, IoTHubServiceClientOptions options)
+        {
+            Argument.AssertNotNull(hostName, nameof(hostName));
+            Argument.AssertNotNull(sharedAccessPolicy, nameof(sharedAccessPolicy));
+            Argument.AssertNotNull(sharedAccessKey, nameof(sharedAccessKey));
+            Argument.AssertNotNull(options, nameof(options));
+
+            var sasTokenProvider = new SasTokenProviderWithSharedAccessKey(
+                hostName,
+                sharedAccessPolicy,
+                sharedAccessKey);
+
+            _endpoint = BuildEndpointUriFromHostName(hostName);
+
+            _clientDiagnostics = new ClientDiagnostics(options);
+
+            options.AddPolicy(new SasTokenAuthenticationPolicy(sasTokenProvider), HttpPipelinePosition.PerCall);
             _httpPipeline = HttpPipelineBuilder.Build(options);
 
             _registryManagerRestClient = new RegistryManagerRestClient(_clientDiagnostics, _httpPipeline, _endpoint, options.GetVersionString());
