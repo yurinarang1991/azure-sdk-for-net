af08d1b14064f9e8bc48192bf2a4d67f82b97e28
Basel Rustum <barustum@microsoft.com>
2020-06-17 14:55:16 -0700

    Initial checkin for the Jobs APIs

--- sdk/iot/Azure.Iot.Hub.Service/src/JobsClient.cs
+++ sdk/iot/Azure.Iot.Hub.Service/src/JobsClient.cs
@@ -4,22 +4,80 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Iot.Hub.Service.Models;
 
 namespace Azure.Iot.Hub.Service
 {
     /// <summary>
-    /// Jobs Client place holder
+    /// Jobs client to support Import/Export and Scheduled Jobs.
     /// </summary>
     public class JobsClient
     {
-        /// <summary>
-        /// place holder
-        /// </summary>
-#pragma warning disable AZC0007 // DO provide a minimal constructor that takes only the parameters required to connect to the service.
-        public JobsClient()
-#pragma warning restore AZC0007 // DO provide a minimal constructor that takes only the parameters required to connect to the service.
-        {
+        private readonly JobRestClient _jobRestClient;
 
+        protected JobsClient()
+        {
+            // This constructor only exists for mocking purposes.
+        }
+
+        internal JobsClient(JobRestClient jobRestClient)
+        {
+            Argument.AssertNotNull(jobRestClient, nameof(jobRestClient));
+
+            _jobRestClient = jobRestClient;
+        }
+
+        /// <summary>
+        /// Creates a job to export device registrations to the container.
+        /// </summary>
+        /// <param name="outputBlobContainerUri">URI containing SAS token to a blob container. This is used to output the results of the export job.</param>
+        /// <param name="excludeKeys">If false, authorization keys are included in export output.</param>
+        /// <param name="options">The optional settings for this request.</param>
+        /// <param name="cancellationToken">Task cancellation token.</param>
+        /// <returns>JobProperties of the newly created job.</returns>
+        public virtual Response<JobProperties> CreateExportDevicesJob(
+            Uri outputBlobContainerUri,
+            bool excludeKeys,
+            JobRequestOptions options = default,
+            CancellationToken cancellationToken = default)
+        {
+            Argument.AssertNotNull(outputBlobContainerUri, nameof(outputBlobContainerUri));
+
+            var jobProperties = new JobProperties
+            {
+                OutputBlobContainerUri = outputBlobContainerUri.ToString(),
+                ExcludeKeysInExport = excludeKeys,
+                StorageAuthenticationType = options?.AuthenticationType,
+                OutputBlobName = options?.BloblName
+            };
+
+            return _jobRestClient.CreateImportExportJob(jobProperties, cancellationToken);
+        }
+
+        /// <summary>
+        /// Creates a job to export device registrations to the container.
+        /// </summary>
+        /// <param name="outputBlobContainerUri">URI containing SAS token to a blob container. This is used to output the results of the export job.</param>
+        /// <param name="excludeKeys">If false, authorization keys are included in export output.</param>
+        /// <param name="options">The optional settings for this request.</param>
+        /// <param name="cancellationToken">Task cancellation token.</param>
+        /// <returns>JobProperties of the newly created job.</returns>
+        public virtual Task<Response<JobProperties>> CreateExportDevicesJobAsync(Uri outputBlobContainerUri, bool excludeKeys, JobRequestOptions options = default, CancellationToken cancellationToken = default)
+        {
+            Argument.AssertNotNull(outputBlobContainerUri, nameof(outputBlobContainerUri));
+
+            var jobProperties = new JobProperties
+            {
+                OutputBlobContainerUri = outputBlobContainerUri.ToString(),
+                ExcludeKeysInExport = excludeKeys,
+                StorageAuthenticationType = options?.AuthenticationType,
+                OutputBlobName = options?.BloblName
+            };
+
+            return _jobRestClient.CreateImportExportJobAsync(jobProperties, cancellationToken);
         }
     }
 }
